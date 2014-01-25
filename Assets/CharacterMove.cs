bd06e68c5dafd1b5621bbad0ff68ee83ac9f95d3
Matthew Kuo <matthewkuo92@gmail.com>
2014-01-24 22:02:54 -0800

    Commited CharacterMove.cs

--- Assets/CharacterMove.cs
+++ Assets/CharacterMove.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class CharacterMove : MonoBehaviour {
 
+	private int speed = 5;
+	private int jumpSpeed = 50;
+	private bool isGrounded = true;
 	// Use this for initialization
 	void Start () {
 	
@@ -11,7 +14,26 @@ public class CharacterMove : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKey (KeyCode.RightArrow)) {
-			transform.Translate(Vector3.right * 3 * Time.deltaTime);
+			transform.Translate(Vector3.right * speed * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			transform.Translate(Vector3.left * speed * Time.deltaTime);
+		}
+
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			transform.Translate(Vector3.up * speed * Time.deltaTime);
 		}
 	}
+
+//	void FixedUpdate () {
+//		if (isGrounded && Input.GetKey (KeyCode.UpArrow)) {
+//			rigidbody2D.AddForce(Vector3.up * jumpSpeed);
+//			isGrounded = false;
+//		}
+//	}
+//
+//	void OnCollisionEnter () {
+//		isGrounded = true;
+//	}
+
 }
