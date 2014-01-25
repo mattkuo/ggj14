4fde526188fecd531aec1ab47f8c4ff0af0fb870
m4rtint <m4rtin.t@gmail.com>
2014-01-25 13:24:16 -0800

    platform level implement death

---
+++ Assets/Death.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Death : MonoBehaviour {
+
+	void OnCollisionEnter2D(Collision2D col){
+		Application.LoadLevel(Application.loadedLevelName);	
+		}
+}
