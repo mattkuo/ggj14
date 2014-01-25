a18c9087fba3b6bec87c40eff5a5b58b9c08791a
Raymond Lee <raylee247@live.com>
2014-01-25 00:22:21 -0800

    force direction script

---
+++ Assets/ForceDirection.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ForceDirection : MonoBehaviour {
+	
+	private int speed = 5;
+	public bool right = false;
+	public bool left = false;
+	public bool up = false;
+	public bool down = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (right) {
+			transform.Translate(Vector3.right * speed * Time.deltaTime);
+		}
+		if (left) {
+			transform.Translate(Vector3.left * speed * Time.deltaTime);
+		}
+		
+		if (up) {
+			transform.Translate(Vector3.up * speed * Time.deltaTime);
+		}
+		if (down) {
+			transform.Translate(Vector3.down * speed * Time.deltaTime);
+		}
+	}
+	
+	
+}
+
