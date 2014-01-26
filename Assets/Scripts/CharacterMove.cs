c868d727884cbcfa26f75956220a55cfacc37e11
Matthew Kuo <matthewkuo92@gmail.com>
2014-01-25 20:05:02 -0800

    I don't remember what I changed

--- Assets/Scripts/CharacterMove.cs
+++ Assets/Scripts/CharacterMove.cs
@@ -1,50 +1,61 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CharacterMove : MonoBehaviour {
+public class CharacterMove : MonoBehaviour
+{
 
-	private int speed = 5;
-	private bool isGrounded = true;
-	private bool jump = false;
-	public float jumpForce = 300f;
-	public static bool isBlackReady = false;
-	public static bool isWhiteReady = false;
-	private GameObject whiteGoal;
-	private GameObject blackGoal;
-	// Use this for initialization
-	void Start () {
-	}
+		private int speed = 5;
+		private bool isGrounded = true;
+		private bool jump = false;
+		public float jumpForce = 300f;
+
+		// Use this for initialization
+
+		void Awake ()
+		{
+		print ("Load player");
+				if (gameObject.name == "black_player") {
+						GameManager.blackPlayer = gameObject;
+				} else {
+						GameManager.whitePlayer = gameObject;
+				}
+		}
+
+		void Start ()
+		{
+				
+		}
 	
-	// Update is called once per frame
-	void Update () {
+		// Update is called once per frame
+		void Update ()
+		{
 
-			if (Input.GetKey (KeyCode.RightArrow)) {
-					transform.Translate (Vector3.right * speed * Time.deltaTime);
-			}
-			if (Input.GetKey (KeyCode.LeftArrow)) {
-					transform.Translate (Vector3.left * speed * Time.deltaTime);
-			}
+				if (Input.GetKey (KeyCode.RightArrow)) {
+						transform.Translate (Vector3.right * speed * Time.deltaTime);
+				}
+				if (Input.GetKey (KeyCode.LeftArrow)) {
+						transform.Translate (Vector3.left * speed * Time.deltaTime);
+				}
 
-			if (Input.GetKey (KeyCode.UpArrow) && isGrounded == true) {
-					jump = true;
-					isGrounded = false;
-			}
-		if (isWhiteReady == true && isBlackReady ==true) {
-			Application.LoadLevel ("tutorial2_jump");
+				if (Input.GetKey (KeyCode.UpArrow) && isGrounded == true) {
+						jump = true;
+						isGrounded = false;
+				}
 		}
-	}
 
-	void FixedUpdate () {
-		if (jump == true) {
-			print ("JUMP");
-			rigidbody2D.AddForce (new Vector2 (0f, jumpForce));
-			jump = false;
+		void FixedUpdate ()
+		{
+				if (jump == true) {
+						print ("JUMP");
+						rigidbody2D.AddForce (new Vector2 (0f, jumpForce));
+						jump = false;
+				}
 		}
-	}
 
-	void OnCollisionEnter2D (Collision2D hit) {
-		//if(hit.gameObject.name == "black_ground")
-		isGrounded = true;
-	}
+		void OnCollisionEnter2D (Collision2D hit)
+		{
+				//if(hit.gameObject.name == "black_ground")
+				isGrounded = true;
+		}
 
 }
