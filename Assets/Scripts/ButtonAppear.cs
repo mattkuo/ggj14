6bded4f6355af5643910b946bc7c4e82bb1d7dc4
n2d8 <09orrp1@gmail.com>
2014-01-25 20:42:59 -0800

    buttin appear

---
+++ Assets/Scripts/ButtonAppear.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonAppear : MonoBehaviour
+{
+
+
+
+		public Sprite activated;
+		public Sprite deactivated;
+		private bool isActivated = false;
+	public GameObject receiver;
+		void Start ()
+		{
+		
+		GetComponent<SpriteRenderer> ().sprite = deactivated;
+		receiver.SetActive (false);
+	}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+			if (isActivated) {
+						receiver.SetActive (true);
+
+				} else {
+			receiver.SetActive(false);
+				}
+			
+			
+
+		}
+		
+
+
+
+
+		void OnCollisionEnter2D (Collision2D collision)
+		{
+			
+			
+						isActivated = true;
+						GetComponent<SpriteRenderer> ().sprite = activated;
+	
+		}
+
+		void OnCollisionExit2D (Collision2D collisionInfo)
+		{
+	
+		isActivated = false;
+		GetComponent<SpriteRenderer> ().sprite = deactivated;
+		}
+}
