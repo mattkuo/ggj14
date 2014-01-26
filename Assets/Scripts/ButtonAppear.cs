413a5f65a36ea3152f0ffec4efe8c0f32e604fdc
Raymond Lee <raylee247@live.com>
2014-01-26 00:01:29 -0800

    interact level

--- Assets/Scripts/ButtonAppear.cs
+++ Assets/Scripts/ButtonAppear.cs
@@ -10,21 +10,55 @@ public class ButtonAppear : MonoBehaviour
 		public Sprite deactivated;
 		private bool isActivated = false;
 	public GameObject receiver;
+	public GameObject receiver1;
+	public GameObject receiver2;
+
+
+	public GameObject turnoff;
+	public GameObject turnoff1;
+	public GameObject turnoff2;
+
 		void Start ()
 		{
 		
 		GetComponent<SpriteRenderer> ().sprite = deactivated;
 		receiver.SetActive (false);
+		receiver1.SetActive (false);
+		receiver2.SetActive (false);
+
+
+
 	}
 	
 		// Update is called once per frame
 		void Update ()
 		{
 			if (isActivated) {
-						receiver.SetActive (true);
+			receiver.SetActive (true);
+			receiver1.SetActive (true);
+			receiver2.SetActive (true);
+
+
+
+
+			turnoff.SetActive (false);
+			turnoff1.SetActive (false);
+			turnoff2.SetActive (false);
+
+
 
 				} else {
-			receiver.SetActive(false);
+			receiver.SetActive (false);
+			receiver1.SetActive (false);
+			receiver2.SetActive (false);
+
+
+
+
+			turnoff.SetActive (true);
+			turnoff1.SetActive (true);
+			turnoff2.SetActive (true);
+
 				}
 			
 			
