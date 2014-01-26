fceb863d6a2bbf87f37ba2d6bdcdf13905d19c17
Matthew Kuo <matthewkuo92@gmail.com>
2014-01-26 00:42:26 -0800

    Added jump for inverted gravity

--- Assets/Scripts/ButtonGravity.cs
+++ Assets/Scripts/ButtonGravity.cs
@@ -3,55 +3,37 @@ using System.Collections;
 
 public class ButtonGravity : MonoBehaviour
 {
-	private bool isGrounded = true;
-	private bool revjump = false;
-	public int gravNum = 1;
-	public int revGravNum = -1;
-
-
-
+		private bool isGrounded = true;
+		private bool revjump = false;
+		public int gravNum = 1;
+		public int revGravNum = -1;
 		public Sprite activated;
 		public Sprite deactivated;
 		private bool isActivated = false;
 		// Use this for initialization
 		public Rigidbody2D player;
-	private bool rev = false;
+		private bool rev = false;
+
 		void Start ()
 		{
 				GetComponent<SpriteRenderer> ().sprite = deactivated;
-		//receiver = GameObject.Find ("white_wall");
+				//receiver = GameObject.Find ("white_wall");
 		}
-	
-		// Update is called once per frame
-		void Update ()
-		{
-			if (isActivated) {
-						rev = true;
-						player.gravityScale = revGravNum;
-					} else {
-			rev = false;
-			player.gravityScale=gravNum;
-				}
-			
-
-		}
-		
-
-
-
 
 		void OnCollisionEnter2D (Collision2D collision)
 		{
-			
-						isActivated = true;
-						GetComponent<SpriteRenderer> ().sprite = activated;
+				isActivated = true;
+				GetComponent<SpriteRenderer> ().sprite = activated;
+				player.gravityScale = revGravNum;
+				player.GetComponent<CharacterMove> ().isGravityReversed = true;
 	
 		}
 
 		void OnCollisionExit2D (Collision2D collisionInfo)
 		{
-	
-		isActivated = false;
-		GetComponent<SpriteRenderer> ().sprite = deactivated;
+				isActivated = false;
+				GetComponent<SpriteRenderer> ().sprite = deactivated;
+				player.gravityScale = gravNum;
+				player.GetComponent<CharacterMove> ().isGravityReversed = false;
 		}
 }
--- Assets/Scripts/CharacterMove.cs
+++ Assets/Scripts/CharacterMove.cs
@@ -8,6 +8,7 @@ public class CharacterMove : MonoBehaviour
 		private bool isGrounded = true;
 		private bool jump = false;
 		public float jumpForce = 300f;
+		public bool isGravityReversed = false;
 
 		// Use this for initialization
 
@@ -29,37 +30,64 @@ public class CharacterMove : MonoBehaviour
 		// Update is called once per frame
 		void Update ()
 		{
+				if (!isGravityReversed) {
 
-				if (Input.GetKey (KeyCode.RightArrow)) {
-						transform.Translate (Vector3.right * speed * Time.deltaTime);
-				}
-				if (Input.GetKey (KeyCode.LeftArrow)) {
-						transform.Translate (Vector3.left * speed * Time.deltaTime);
-				}
+						if (Input.GetKey (KeyCode.RightArrow)) {
+								transform.Translate (Vector3.right * speed * Time.deltaTime);
+						}
+						if (Input.GetKey (KeyCode.LeftArrow)) {
+								transform.Translate (Vector3.left * speed * Time.deltaTime);
+						}
 
-				if (Input.GetKey (KeyCode.UpArrow) && isGrounded == true) {
-						jump = true;
-						isGrounded = false;
+						if (Input.GetKey (KeyCode.UpArrow) && isGrounded == true) {
+								jump = true;
+								isGrounded = false;
+						}
+				} else {
+						if (Input.GetKey (KeyCode.RightArrow)) {
+								transform.Translate (Vector3.right * speed * Time.deltaTime);
+						}
+						if (Input.GetKey (KeyCode.LeftArrow)) {
+								transform.Translate (Vector3.left * speed * Time.deltaTime);
+						}
+			
+						if (Input.GetKey (KeyCode.DownArrow) && isGrounded == true) {
+								jump = true;
+								isGrounded = false;
+						}
 				}
 		}
-
+		
 		void FixedUpdate ()
 		{
 				if (jump == true) {
-						print ("JUMP");
-						rigidbody2D.AddForce (new Vector2 (0f, jumpForce));
+						if (!isGravityReversed) {
+								rigidbody2D.AddForce (new Vector2 (0f, jumpForce));
+						} else {
+								rigidbody2D.AddForce (new Vector2 (0f, -1.0f * jumpForce));
+						
+						}
 						jump = false;
 				}
 		}
 
 		void OnCollisionEnter2D (Collision2D hit)
 		{
-				print (hit.gameObject.name);
-				for (int i=0; i<hit.contacts.Length; i++) {
-						if (hit.contacts [i].normal.y > 0) {
-								print ("Hit blackground");
-								isGrounded = true;
+		print (hit.gameObject.name);
+				if (!isGravityReversed) {
+
+						for (int i=0; i<hit.contacts.Length; i++) {
+								if (hit.contacts [i].normal.y > 0) {
+										isGrounded = true;
+								}
+						}
+				} else {
+						for (int i=0; i<hit.contacts.Length; i++) {
+								if (hit.contacts [i].normal.y < 0) {
+										isGrounded = true;
+								}
 						}
 				}
 		}
 }
+	
