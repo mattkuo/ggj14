a225efa0c2fd146bf2060a4b58a581fbe7326a56
m4rtint <m4rtin.t@gmail.com>
2014-01-25 18:13:22 -0800

    Revert "change plat level with spikes"
    This reverts commit d75271745ae8eac0c0e1ff44416c361d4fd97fbf.

---
+++ Assets/Scripts/PlatePressed.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatePressed : MonoBehaviour
+{
+		public Sprite activated;
+		public Sprite deactivated;
+		private bool isActivated = false;
+		//public Transform DestinationSpot;
+
+		public GameObject receiver;
+
+		// Use this for initialization
+		void Start ()
+		{
+				GetComponent<SpriteRenderer> ().sprite = deactivated;
+		//receiver = GameObject.Find ("white_wall");
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+			if (isActivated) {
+				
+			Destroy(receiver);
+		}
+		}
+
+		void OnCollisionEnter2D (Collision2D collision)
+		{
+			
+			
+						isActivated = true;
+
+						GetComponent<SpriteRenderer> ().sprite = activated;
+				
+		}
+
+		void OnCollisionExit2D (Collision2D collisionInfo)
+		{
+				GetComponent<SpriteRenderer> ().sprite = deactivated;
+		}
+}
