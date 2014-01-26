8dd3b089697ff891040fa5a136d24c46cbdedf15
Matthew Kuo <matthewkuo92@gmail.com>
2014-01-25 22:02:11 -0800

    Semi-fixed jump

---
+++ Assets/JumpObject.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JumpObject : MonoBehaviour
+{
+
+		private bool isGrounded = true;
+		private bool jump = false;
+		public float jumpForce = 300f;
+
+		// Use this for initialization
+		void Start ()
+		{
+	
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				if (Input.GetKey (KeyCode.UpArrow) && isGrounded == true) {
+						jump = true;
+						isGrounded = false;
+				}
+		}
+
+		void FixedUpdate ()
+		{
+				if (jump == true) {
+						rigidbody2D.AddForce (new Vector2 (0f, jumpForce));
+						jump = false;
+				}
+		}
+
+		void OnCollisionEnter2D (Collision2D hit)
+		{
+				
+				isGrounded = true;
+
+		}
+}
